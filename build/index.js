0cc30f53acb3281e5295ac8c65dee6238b82cf28
konstantin23 <kostharl@gmail.com>
2017-07-18 20:40:01 +0300

    Execution file was updated

--- build/index.js
+++ build/index.js
@@ -1,25 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Created by kokh0716 on 6/27/2017.
- */
 var dist_1 = require("ioc-lib.js/dist");
 var User_1 = require("./entities/User");
 var Service_1 = require("./services/Service");
 var path = require('path');
+//Paths to the configuration files with metadata
 var configs = [__dirname + "/configs/context.json"];
+//Creating application context based on metadata
 var context = new dist_1.MetadataContext(configs);
+//Providing safe close for the application context
 context.registerShutdownHook();
-var userConf = context.getComponentEntityInstance('user');
-// const config = new UserConfig();
-//
-// let user = config.getUserComponent();
+//Getting user's component instance from the application context
+var userFromContext = context.getComponentEntityInstance('user');
+//Creeating simple object of Service class
 var service = new Service_1.default("Test service", 1455);
-//
+//Creating simple object of User class and inject service via constructor
 var user = new User_1.default("Darthven", 25, ["066-240-52-63"], service);
-// Object.keys(User.prototype).forEach((key) => {
-//     console.log(key);
-// });
-console.log(userConf);
-console.log(user);
-//console.log(user['getClassPath'].call()) 
+console.log("User created by the application context: " + userFromContext);
+console.log("User created by own hands: " + user);
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,25 @@
-/**
- * Created by kokh0716 on 6/27/2017.
- */
 import {MetadataContext} from 'ioc-lib.js/dist'
 import User from "./entities/User";
 import Service from "./services/Service";
-
 const path = require('path');
 
+//Paths to the configuration files with metadata
 const configs = [__dirname + "/configs/context.json"];
 
+//Creating application context based on metadata
 let context = new MetadataContext(configs);
+
+//Providing safe close for the application context
 context.registerShutdownHook();
 
-let userConf = context.getComponentEntityInstance('user');
+//Getting user's component instance from the application context
+let userFromContext = context.getComponentEntityInstance('user');
 
-
-
-// const config = new UserConfig();
-//
-// let user = config.getUserComponent();
-
+//Creeating simple object of Service class
 let service = new Service("Test service", 1455);
-//
+
+//Creating simple object of User class and inject service via constructor
 let user = new User("Darthven", 25, ["066-240-52-63"], service);
 
-// Object.keys(User.prototype).forEach((key) => {
-//     console.log(key);
-// });
-
-console.log(userConf);
-console.log(user);
-
-
-
-
-//console.log(user['getClassPath'].call())
+console.log(`User created by the application context: ${userFromContext}`);
+console.log(`User created by own hands: ${user}`);
